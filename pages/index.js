8d3f1eb4c03b6f515f64c88fbf79ea3978abf6f2
GarvCoding <115489001+GarvCoding@users.noreply.github.com>
2022-10-19 21:34:48 +0530

    Test features

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,17 @@ export default function Home() {
   const date = new Date();
   const currTime = date.getHours();
 
+  const [dayMode, setDayMode] = useState(false)
+  const changeMode = () => {
+    (!dayMode ? setDayMode(true) : setDayMode(false))
+    console.log(dayMode)
+  }
 
   const [showResults, setShowResults] = useState(false)
   const showMore = () => setShowResults(true)
+
+  const [showAll, setShowAll] = useState(false)
+  const showRest = () => setShowAll(true)
   return (
     <div className={styles.container}>
       <Head>
@@ -19,8 +27,17 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
 
-        <h1>Hello, I'm <button onClick={showMore}>Garv</button></h1>
-        {showResults ? <Results /> : null}
+        <p><button onClick={changeMode}>Namastey <div className='btn'>🙏</div></button></p>
+
+        <h1>I'm <button className='name' onClick={showMore}>Garv Pandey</button></h1>
+        {showResults ? <div id="results" className="search-results">
+          <p>I'm a Computer Science Engineer mostly into making Web-Applications<button className='normal-btn' onClick={showRest}>Know More About me</button></p>
+        </div> : null}
+        {showAll ? <div id="allContent" className='all-rest'>
+          <p>I'm a Computer Science Engineer graduated from GD Goenka University in 2022</p>
+        </div> : null}
+
+
       </main>
 
     </div>
@@ -28,8 +45,5 @@ export default function Home() {
 }
 
 
-const Results = () => (
-  <div id="results" className="search-results">
-    <p>I'm a Computer Science Engineer Graduated from G.D Goenka University in 2022</p>
-  </div>)
+
 
